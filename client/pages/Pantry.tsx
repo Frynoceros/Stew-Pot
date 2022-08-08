c0aee9f71f3d3f588b6e6727b799201b3e950181
Your Namejames <jamesmbui1999@gmail.com>
2022-08-08 16:45:48 -0700

    finished building react router

---
+++ client/pages/Pantry.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+const Pantry = () => {
+  return (
+    <div> Pantry page </div>
+  )
+}
+
+export default Pantry;
---
+++ client/pages/Signup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+const Signup = () => {
+  return (
+    <div> Signup page </div>
+  )
+}
+
+export default Signup;
