50986a7d64f3250d824be718f97003151ebbf199
Gabriel <gkime91@gmail.com>
2022-08-08 15:50:29 -0700

    finished out stewpot boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/App.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.jsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: './index.html',
+    }),
+   /* new CopyPlugin({
+      patterns: [{ from: './client/style.css' }],
+    }), */
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    proxy: {
+      '/api': 'http://localhost:3000',
+      secure: false
+    }
+  },
+}
