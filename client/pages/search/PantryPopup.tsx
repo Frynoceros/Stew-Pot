d9401b0ff0ff06650645e2a65759563f0937e4d5
Carol Xia <c.xia.98@gmail.com>
2022-08-09 13:13:57 -0700

    working on pantry popup

--- client/pages/search/PantryPopup.tsx
+++ client/pages/search/PantryPopup.tsx
@@ -2,6 +2,22 @@ import React, { useState } from 'react';
 import { Container, Grid, Paper, styled, ButtonBase, Typography, Icon } from '@mui/material';
 
 const PantryPopup = (props) => {
+
+  const useStyles = makeStyles(theme => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        position: 'absolute',
+        width: 450,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
   return (
     <Container id="pantry-popup"> 
       {/* <link rel="stylesheet" href="./search.css" /> */}
