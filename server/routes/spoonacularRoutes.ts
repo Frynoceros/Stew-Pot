d270b2473d58b6e867ef8ee66ad288036734797a
Gabriel <gkime91@gmail.com>
2022-08-09 15:22:48 -0700

    wrote webapi route and controller v1.

--- server/routes/spoonacularRoutes.ts
+++ server/routes/spoonacularRoutes.ts
@@ -3,18 +3,17 @@ import {Request, Response, NextFunction } from 'express'
 // should we use request or request promise or axios? Axios has support going forwards, whereas request has been put into maintenance mode. 
 // axios also supports async await, and has browser support. 
 import express from 'express';
-import spoonacularRoutes from '../controllers/spoonacularControllers'
-
+import { spoonController } from '../controllers/spoonacularControllers';
 // Routes for the spoonacular recipe api
 
-const router = express.Router();
+const webApiRouter = express.Router();
 // ===
 // GET
 // ===
 // get all recipes(that match request body)
-router.get('/recipes', (req: Request , res: Response, next: NextFunction) => {
-  
-})
+webApiRouter.get('/recipes', spoonController, (req, res, next) => {
+  res.status(200);
+});
 
 // get one recipe
 
